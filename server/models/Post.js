5a41cf9916a15f6f976ca6f3e936e631e36999e1
unknown <lssuseendharlal@gmail.com>
2020-12-29 18:15:46 +0530

    timestamps added

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,26 +1,29 @@
 const { model, Schema } = require("mongoose");
 
-const postSchema = new Schema({
-  image: {
-    type: String,
-    required: [true, "Image required"],
+const postSchema = new Schema(
+  {
+    image: {
+      type: String,
+      required: [true, "Image required"],
+    },
+    userId: {
+      type: String,
+      required: [true, "Id required"],
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    comments: {
+      type: [{ user: String, comment: String }],
+      default: [],
+    },
+    title: {
+      type: String,
+      required: [true, "title required"],
+    },
   },
-  userId: {
-    type: String,
-    required: [true, "Id required"],
-  },
-  likes: {
-    type: [String],
-    default: [],
-  },
-  comments: {
-    type: [{ user: String, comment: String }],
-    default: [],
-  },
-  title: {
-    type: String,
-    required: [true, "title required"],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("Post", postSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,21 @@
 const { model, Schema } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "username Required"],
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username Required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email Required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password Required"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "email Required"],
-  },
-  password: {
-    type: String,
-    required: [true, "password Required"],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("User", userSchema);
